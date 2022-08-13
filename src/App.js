1c2110df1ea2ee337fb4fb3936af73ee30f415aa
aditya12gusain <adisandi3112002@gmail.com>
2022-08-13 16:49:43 +0530

    Completed all tasks

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { Routes, Route } from 'react-router-dom';
+import Cart from './pages/Cart';
+import Products from './pages/Products';
+import './styles.css';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <Routes>
+      <Route path="/" element={<Products />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
   );
 }
 
---
+++ src/components/ProductsList.jsx
@@ -0,0 +1,32 @@
+import { Grid, Text } from '@chakra-ui/react';
+import React from 'react';
+import { DefaultState } from '../context/DefaultContext';
+import ProductCard from './common/ProductCard';
+
+const ProductsList = () => {
+  const { products, searchResult, searchString } = DefaultState();
+
+  return (
+    <Grid
+      templateColumns={{
+        base: 'repeat(1, 1fr)',
+        md: 'repeat(3, 1fr)',
+        lg: 'repeat(5, 1fr)',
+      }}
+      gap={5}
+      margin={'40px 0'}
+    >
+      {searchString.length > 0 || searchResult ? (
+        searchResult.length === 0 ? (
+          <Text fontSize={20}>No Product Found</Text>
+        ) : (
+          searchResult.map(data => <ProductCard key={data.id} data={data} />)
+        )
+      ) : (
+        products.map(data => <ProductCard key={data.id} data={data} />)
+      )}
+    </Grid>
+  );
+};
+
+export default ProductsList;
